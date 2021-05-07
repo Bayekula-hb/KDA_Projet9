69a81a66399806e33abcbf9e102095d4318e1d8b
Bayekula-hb <hobedbayekula@gmail.com>
2021-05-07 08:32:49 +0100

    refactor add useContext to section comedy

--- src/app.jsx
+++ src/app.jsx
@@ -11,6 +11,7 @@ import Footer from "./components/Footer";
 export const filmPopularContext = React.createContext();
 export const filmActionContext = React.createContext();
 export const filmDramaContext = React.createContext();
+export const filmComdeyContext = React.createContext();
 
 function App() {
   const [listFilmPopular, setListFilm] = useState([]);
@@ -43,10 +44,11 @@ function App() {
   let TabFilmAction = [];
   listFilmAction.map((film, index) => {
     for (const Id of film.genre_ids) {
-      if ((Id === 18 || Id === 12) && Id != 10749) {
+      if ((Id === 18 || Id === 12) && Id !== 10749) {
         TabFilmAction.push(film);
       }
     }
+    return "";
   });
   const filmAction = TabFilmAction.filter((film, index) => {
     return index < 4;
@@ -67,13 +69,41 @@ function App() {
   let TabFilmDrama = [];
   listFilmDrama.map((film) => {
     for (const Id of film.genre_ids) {
-      if (Id === 18 || (Id === 10749 && Id != 12 && Id != 28)) {
+      if (Id === 18 || (Id === 10749 && Id !== 12 && Id !== 28)) {
         TabFilmDrama.push(film);
       }
     }
+    return "";
   });
   const filmDrama = TabFilmDrama.filter((film, index) => {
     return index >= 0 && index < 4;
+  });
+
+
+  const [listFilmComdey, setlistFilmComdey] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/list/20?api_key=b5be08aabb5691e1522a3bd74604d1ee&page=1"
+      )
+      .then((result) => {
+        return result.data.items;
+      })
+      .then((films) => {
+        setlistFilmComdey(films);
+      });
+  },[]);
+  let TabFilmComedy = [];
+  listFilmComdey.map((film)=>{
+    for (const Id of film.genre_ids) {
+      if((Id === 35 || Id===9648) && (Id !== 12 && Id !== 10749) ){
+        TabFilmComedy.push(film)
+      }
+    }
+    return "";
+  })
+  const filmComedy = TabFilmComedy.filter((film, index) => {
+    return index < 4;
   });
 
   return (
@@ -83,7 +113,9 @@ function App() {
         <filmPopularContext.Provider value={listFilmPopular}>
           <filmActionContext.Provider value={filmAction}>
             <filmDramaContext.Provider value={filmDrama}>
-              <Route exact path="/" component={MainHome}></Route>
+              <filmComdeyContext.Provider value={filmComedy}>
+                <Route exact path="/" component={MainHome}></Route>
+              </filmComdeyContext.Provider>
             </filmDramaContext.Provider>
           </filmActionContext.Provider>
           <Route path="/movies" component={Movies}></Route>
--- src/components/Home/sectionComedy.jsx
+++ src/components/Home/sectionComedy.jsx
@@ -1,36 +1,13 @@
 
-import { useState, useEffect } from "react";
-import axios from "axios"
+import { useContext } from "react";
 import Card from "../Card/card";
 import Ul, { Li } from "../Ul";
 import { Link } from "react-router-dom";
+import {filmComdeyContext} from "../../app";
 
 const SectionComedy = () => {
-   const [listFilm, setListFilm] = useState([]);
   const linkImage = "https://www.themoviedb.org/t/p/w220_and_h330_face/";
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.themoviedb.org/3/list/20?api_key=b5be08aabb5691e1522a3bd74604d1ee&page=1"
-      )
-      .then((result) => {
-        return result.data.items;
-      })
-      .then((films) => {
-        setListFilm(films);
-      });
-  },[]);
-  let TabFilmComedy = [];
-  listFilm.map((film)=>{
-    for (const Id of film.genre_ids) {
-      if(Id === 35 || Id===9648 && Id !== 12 && Id != 10749 ){
-        TabFilmComedy.push(film)
-      }
-    }
-  })
-  const filmComedy = TabFilmComedy.filter((film, index) => {
-    return index < 4;
-  });
+  const filmComedy = useContext(filmComdeyContext)
   return (
     <section className="section section--action">
       <h2 className="h2 h2--titleSection">Comedy & Mystery</h2>
